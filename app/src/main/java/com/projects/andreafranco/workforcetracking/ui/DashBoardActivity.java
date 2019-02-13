26d9872c09e929b95c2ed3f6f22f3b1fa9e2da3e
Andrea Franco <andrea.franco@overit.it>
2019-02-13 18:18:34 +0100

    Added drawermenu

--- app/src/main/java/com/projects/andreafranco/workforcetracking/ui/DashBoardActivity.java
+++ app/src/main/java/com/projects/andreafranco/workforcetracking/ui/DashBoardActivity.java
@@ -1,13 +1,25 @@
 package com.projects.andreafranco.workforcetracking.ui;
 
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.graphics.drawable.DrawerArrowDrawable;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
 
 import com.projects.andreafranco.workforcetracking.R;
 
 import java.util.ArrayList;
 
-public class DashBoardActivity extends SingleFragmentActivity implements DashBoardFragment.OnDashBoardFragmentInteractionListener {
+public class DashBoardActivity extends SingleFragmentActivity implements DashBoardFragment.OnDashBoardFragmentInteractionListener, NavigationView.OnNavigationItemSelectedListener {
 
     private int mUserId;
     private ArrayList<Integer> mMenuImages;
@@ -18,8 +30,78 @@ public class DashBoardActivity extends SingleFragmentActivity implements DashBoa
         if (extras != null) {
             mUserId = extras.getInt(LoginFragment.USER_ID);
         }
-        setTitle(R.string.dashboard_title);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+
+        setUpNavigationDrawer();
+    }
+
+    private void setUpNavigationDrawer() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.dashboard_toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        final NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        final View contentView = findViewById(R.id.content);
+
+        toolbar.setNavigationIcon(new DrawerArrowDrawable(this));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                                                 @Override
+                                                 public void onClick(View v) {
+                                                     if (drawerLayout.isDrawerOpen(navigationView)) {
+                                                         drawerLayout.closeDrawer(navigationView);
+                                                     }
+                                                     else {
+                                                         drawerLayout.openDrawer(navigationView);
+                                                     }
+                                                 }
+                                             }
+        );
+
+        drawerLayout.setScrimColor(Color.TRANSPARENT);
+        drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+                                           @Override
+                                           public void onDrawerSlide(View drawerView, float slideOffset) {
+
+                                               // Scale the View based on current slide offset
+                                               final float diffScaledOffset = slideOffset * (1 - 0.5f);
+                                               final float offsetScale = 1 - diffScaledOffset;
+                                               contentView.setScaleX(offsetScale);
+                                               contentView.setScaleY(offsetScale);
+
+                                               // Translate the View, accounting for the scaled width
+                                               final float xOffset = drawerView.getWidth() * slideOffset;
+                                               final float xOffsetDiff = contentView.getWidth() * diffScaledOffset / 2;
+                                               final float xTranslation = xOffset - xOffsetDiff;
+                                               contentView.setTranslationX(xTranslation);
+                                           }
+
+                                           @Override
+                                           public void onDrawerClosed(View drawerView) {
+                                           }
+                                       }
+        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_dashboard;
     }
 
     @Override
@@ -29,13 +111,35 @@ public class DashBoardActivity extends SingleFragmentActivity implements DashBoa
 
     @Override
     protected void setupToolbar(boolean visible) {
-        //Use binding for toolbar or menu with user info and profile
-        getSupportActionBar().setDisplayHomeAsUpEnabled(visible);
-        getSupportActionBar().setDisplayShowHomeEnabled(visible);
     }
 
     @Override
     public void onFragmentInteraction() {
         //TODO actions
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
