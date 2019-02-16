9f20461c53b390c68fd45cde3885d018dd08ab06
andreafranco <JMnh07021981nP>
2019-02-16 15:45:28 +0100

    Added Custom image creation

---
+++ app/src/main/java/com/projects/andreafranco/workforcetracking/TeamManagementActivity.java
@@ -0,0 +1,13 @@
+package com.projects.andreafranco.workforcetracking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeamManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_management);
+    }
+}
