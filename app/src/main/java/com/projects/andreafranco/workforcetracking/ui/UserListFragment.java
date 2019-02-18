a60d55b704b426cb4efa1b26e61dced2ed6fb4e5
Andrea Franco <andrea.franco@overit.it>
2019-02-18 19:06:32 +0100

    Added user list

--- app/src/main/java/com/projects/andreafranco/workforcetracking/ui/UserListFragment.java
+++ app/src/main/java/com/projects/andreafranco/workforcetracking/ui/UserListFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -55,7 +56,11 @@ public class UserListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_list, container, false);
+        View view =  inflater.inflate(R.layout.fragment_user_list, container, false);
+        RecyclerView userListRecycleView = view.findViewById(R.id.userlist_recycleview);
+        //TODO create and add the adapter witn cardview
+
+        return view;
     }
 
     @Override
